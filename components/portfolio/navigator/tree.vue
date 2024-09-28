e360cb9c3928efd97d2b1c9a04f42b94cb9a0490
Jura Semenov <info@wstudiou.ru>
2024-09-29 02:34:40 +0300

    Init

--- components/portfolio/navigator/tree.vue
+++ components/portfolio/navigator/tree.vue
@@ -16,6 +16,7 @@ const props = withDefaults(defineProps<Props>(), {
 const { ui, attrs } = useCore('portfolio-navigator-tree', toRef(props, 'ui'), config, toRef(props, 'class'))
 
 const route = useRoute()
+// @ts-ignore
 const project_id = computed(() => String(route.params?.project_id))
 
 const getLinkProps = (_entry: Portfolios.NavigatorItem) => {
